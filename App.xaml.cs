a243c1628b574aa90a5a8e6af300da6bbbb84dce
Phan Long <phanmainhatlong@outlook.com>
2026-06-26 16:17:35 +0700

    Configure dependencies and setup ServiceProvider

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,35 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
-namespace QuanLyNhaHang
+using Microsoft.Extensions.DependencyInjection;
+
+namespace QuanLyNhaHang;
+
+/// <summary>
+/// Main application shell
+/// </summary>
+public partial class App : Application
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// Gets the current <see cref="App"/> instance in use
     /// </summary>
-    public partial class App : Application
+    public new static App Current = (App)Application.Current;
+    /// <summary>
+    /// Gets the <see cref="IServiceProvider"/> instance to resolve application services.
+    /// </summary>
+    public IServiceProvider Services { get; }
+    /// <summary>
+    /// Configures the services for the application.
+    /// </summary>
+    private static IServiceProvider ConfigureServices()
     {
+        var services = new ServiceCollection();
+        return services.BuildServiceProvider();
     }
 
+    public App()
+    {
+        Services = ConfigureServices();
+
+        InitializeComponent();
+    }
 }
